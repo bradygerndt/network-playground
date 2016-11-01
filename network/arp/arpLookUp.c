35fa245dc10b5863b5d82e7ad187be0bf4da76aa
Brady <brady.gerndt@gmail.com>
2016-11-01 08:00:35 -0500

    Added arpLookup

---
+++ network/arp/arpLookUp.c
@@ -0,0 +1,26 @@
+#include <xinu.h>
+
+
+
+
+//Should probably check by MAC rather than IP.
+
+int arpLookUp(uchar ipaddr[IP_ADDR_LEN])
+{
+  wait(sem);
+  for (i = 0; i < ARP_NUM_ENTRY; i++)
+  {
+    if(ipaddr == arptab[i].praddr)
+    {
+      mac = arptab[i].hwaddr;
+      arptab[i].expires = clocktime + 1800;
+      signal(sem);
+      return TRUE;
+      }
+    else
+    {
+      return FALSE;
+    }
+    signal(sem);
+  }
+}
