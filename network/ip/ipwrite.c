73b2d0c8c272c60e9949078af82405a3e69ac55f
Brady <brady.gerndt@gmail.com>
2016-11-22 17:06:23 -0600

    updated ip

--- network/ip/ipwrite.c
+++ network/ip/ipwrite.c
@@ -1,18 +1,24 @@
 #include <xinu.h>
 
-syscall ipwrite(uchar *payload, int length, int protocol)
+syscall ipwrite(uchar *payload, int length, int protocol, uchar dstIp[IP_ADDR_LEN])
 {
+  uchar srcIp[IP_ADDR_LEN];
+
+  dot2ip(nvramGet("lan_ipaddr\0"), srcIp);
+
   struct ipgram *ip;
 
   ip = (struct ipgram *) malloc(sizeof(struct ipgram) + length);
-  ip->ver_ihl = //Fill in with version and header length
+  ip->ver_ihl = (char) (IPv4_VERSION << 4);
+  ip->ver_ihl += (IPv4_MIN_IHL); //Fill in with version and header length
   ip->tos = IPv4_TOS_ROUTINE;
-  ip->len = length + //add header length
-  ip->id = //arbitrary;
+  ip->len = length + IPv4_HDR_LEN;//add header length
+  ip->id = 0;
   ip->flags_froff = 0;
   ip->ttl = IPv4_TTL;
-  ip->proto =
-  ip
+  ip->proto = protocol;
+  ip->chksum = checksum(&ip, IPv4_HDR_LEN);
+  ip->src = srcIp;
 
 
 
