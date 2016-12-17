32c313289ff65d8963822ee9394463e968c48410
Brady Gerndt <bradygerndt@Bradys-MacBook-Pro.local>
2016-12-16 19:36:08 -0600

    writing to ethernet in netwrite

--- network/ip/netwrite.c
+++ network/ip/netwrite.c
@@ -2,8 +2,24 @@
 
 syscall netwrite(uchar mac[ETH_ADDR_LEN], struct ipgram *ip)
 {
+  int lenWritten;
+  uchar srcMac[ETH_ADDR_LEN];
+  struct ethergram *egram = NULL;
+  egram = (struct ethergram *) malloc(PKTSZ);
 
-  fprintf(CONSOLE, "Netwrite MAC --> %02x:%02x:%02x:%02x:%02x:%02x\n", mac[0], mac[1],mac[2], mac[3], mac[4], mac[5], mac[ETH_ADDR_LEN]);
+  control(ETH0, ETH_CTRL_GET_MAC, (ulong) srcMac, 0);
 
+  memcpy(&egram->dst, &mac, ETH_ADDR_LEN);
+  memcpy(&egram->src, &srcMac, ETH_ADDR_LEN);
+  egram->type = ETYPE_IPv4;
+  memcpy(egram->data, ip, sizeof(ip));
+
+  if((lenWritten = write(ETH0, egram, ETH_MTU)) == SYSERR)
+  {
+    fprintf(CONSOLE, "%s\n", "Packet failed to send");
+    return SYSERR;
+  }
+
+  return OK;
 
 }
